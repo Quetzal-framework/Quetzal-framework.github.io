0916e90e5f65dbf503ae85bc351493df129c5d57
Becheler <Becheler@users.noreply.github.com>
2022-07-27 20:56:01 +0000

    deploy: 654c5ab3f19bd3c76e94babf1a98773b92b532d6

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['welcome_20to_20the_20quetzal_20framework_27s_20website_0',['Welcome to the Quetzal framework&apos;s website',['../index.html',1,'']]]
-];
--- main-page.md
+++
@@ -1,1 +0,0 @@
-# Welcome to the Quetzal framework's website
